176d3d2892b846d21419f70ba4b0cdc03cce6dd5
khaled39ab <khaled39ab@gmail.com>
2023-03-08 01:05:14 +0600

    it was a hororable day to me. project won't be run. now it's working

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1 className="text-4xl font-bold underline text-red-600">
+        Hello world!
+      </h1>
     </div>
   );
 }
