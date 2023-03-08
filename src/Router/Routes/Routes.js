c7fcefa60ce9b8d8606e943b63f2cfd799ec3c41
khaled39ab <khaled39ab@gmail.com>
2023-03-08 23:10:10 +0600

    starcture done

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home";
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home />
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
     }
 ]);
 
