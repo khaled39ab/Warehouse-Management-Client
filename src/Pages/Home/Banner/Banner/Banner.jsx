185102be4feedf714ef7bd8a1f3a9f527fca71f1
khaled39ab <khaled39ab@gmail.com>
2023-03-10 23:47:00 +0600

    carousel section done

---
+++ src/Pages/Home/Banner/Banner/Banner.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import img1 from '../../../../asset/images/banner/car-banner1.jpg';
+import img2 from '../../../../asset/images/banner/car-banner2.jpg';
+import img3 from '../../../../asset/images/banner/car-banner3.jpg';
+import img4 from '../../../../asset/images/banner/car-banner4.jpg';
+import Carousel from '../Carousel/Carousel';
+
+const Banner = () => {
+    const bannerData = [
+        {
+            id: 1,
+            prev: 6,
+            next: 2,
+            image: img1
+        },
+        {
+            id: 2,
+            prev: 1,
+            next: 3,
+            image: img2
+        },
+        {
+            id: 3,
+            prev: 2,
+            next: 4,
+            image: img3
+        },
+        {
+            id: 4,
+            prev: 3,
+            next: 1,
+            image: img4
+        }
+    ]
+    return (
+        <div className="carousel w-full">
+            {
+                bannerData.map(slide => <Carousel
+                    key={slide.id}
+                    slide={slide}
+                />)
+            }
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Banner/Carousel/Carousel.jsx
+++ src/Pages/Home/Banner/Carousel/Carousel.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Carousel = () => {
+const Carousel = ({slide}) => {
+    const {id, image, prev, next} = slide;
+
     return (
-        <div id="slide1" className="carousel-item relative w-full">
-            <img src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+            <img src={image} alt='' className="w-full" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide4" className="btn btn-circle">❮</a>
-                <a href="#slide2" className="btn btn-circle">❯</a>
+                <a href={`#slide${prev}`} className="btn btn-circle">❮</a>
+                <a href={`#slide${next}`} className="btn btn-circle">❯</a>
             </div>
         </div>
     );
