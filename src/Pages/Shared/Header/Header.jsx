38db4749a3cf31e2a4d77f5abf9cd7c490560d0a
khaled39ab <khaled39ab@gmail.com>
2023-03-11 23:58:19 +0600

    header change

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -11,8 +11,8 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link>Item 1</Link></li>
-                        <li><Link>Item 2</Link></li>
+                        <li><Link to={'/store'}>Store</Link></li>
+                        <li><Link to={'/blogs'}>Blogs</Link></li>
                         {/* <li tabIndex={0}>
                             <Link className="justify-between">
                                 Parent
@@ -23,7 +23,7 @@ const Header = () => {
                                 <li><Link>Submenu 2</Link></li>
                             </ul>
                         </li> */}
-                        <li><Link>Item 3</Link></li>
+                        <li><Link to={'/about'}>About us</Link></li>
                     </ul>
                 </div>
                 <Link to={'/'} className="btn btn-ghost normal-case text-xl">
@@ -33,8 +33,8 @@ const Header = () => {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link>Item 1</Link></li>
-                    <li><Link>Item 2</Link></li>
+                    <li><Link to={'/store'}>Store</Link></li>
+                    <li><Link to={'/blogs'}>Blogs</Link></li>
                     {/* <li tabIndex={0}>
                         <Link>
                             Parent
@@ -45,7 +45,7 @@ const Header = () => {
                             <li><Link>Submenu 2</Link></li>
                         </ul>
                     </li> */}
-                    <li><Link>Item 3</Link></li>
+                    <li><Link to={'/about'}>About Us</Link></li>
                 </ul>
             </div>
             <div className="navbar-end">
