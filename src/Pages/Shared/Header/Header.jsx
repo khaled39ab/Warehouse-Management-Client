2d4259f51299dbd04da2edcb6393519dbee3eec3
khaled39ab <khaled39ab@gmail.com>
2023-03-14 22:35:41 +0600

    handle auth state change and display user info

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -7,8 +7,11 @@ const Header = () => {
 
     const { user, logOut } = useContext(AuthContext);
 
-    if(user){
-        console.log(user);
+    const name = (user?.displayName)?.split(' ')[0];
+    const img = user?.photoURL;
+
+    const handleSignOut = () => {
+        logOut();
     }
 
     return (
@@ -57,7 +60,22 @@ const Header = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <Link to={'/signIn'} className="btn">Sign In </Link>
+                {
+                    user ?
+                        <div className="dropdown dropdown-bottom dropdown-end">
+                            <label tabIndex={0} className="btn btn-outline rounded-btn">
+                                <img className='h-8 rounded-full mr-2' src={img} alt="" />
+                                {name}
+                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                            </label>
+                            <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-1 font-bold">
+                                {/* <li><Link to={'/orders'}>Orders</Link></li> */}
+                                <li><button onClick={handleSignOut} className="btn  btn-secondary">Log Out</button></li>
+                            </ul>
+                        </div>
+                        :
+                        <Link to={'/signIn'} className="btn btn-outline btn-secondary">Sign In </Link>
+                }
             </div>
         </div>
     );
