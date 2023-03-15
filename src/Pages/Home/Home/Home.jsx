62af06d0f46954a10057ef791866c05594ba3d34
khaled39ab <khaled39ab@gmail.com>
2023-03-15 15:29:16 +0600

    create custom hooks to fetch data

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner/Banner';
+import Items from '../Items/Items/Items';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Items />
         </div>
     );
 };
--- src/Pages/Home/Items/Items/Items.jsx
+++ src/Pages/Home/Items/Items/Items.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import useFetch from '../../../../hooks/useFetch/useFetch';
 
 const Items = () => {
+    const {data} = useFetch('https://jsonplaceholder.typicode.com/users');
+    console.log(data);
+
+    
     return (
         <div>
             Items
