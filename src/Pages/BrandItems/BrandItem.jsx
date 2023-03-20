67dfea3e4dccb650b84adfc2440d44a39bb942ff
khaled39ab <khaled39ab@gmail.com>
2023-03-21 00:09:49 +0600

    display specific brands item

---
+++ src/Pages/BrandItems/BrandItem.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const BrandItem = ({ item }) => {
+
+
+    const { _id, company_name, car_model, car_color, model_year, car_price, quantity, provider_name, provider_email, photo_url, car_vin, description } = item;
+
+    return (
+        <div className='m-20 rounded-2xl' data-theme="cupcake">
+            <div className="card md:card-side bg-green-100 shadow-xl">
+                <div className='md:m-10 rounded-xl'>
+                    <figure><img src={photo_url} alt="Movie" /></figure>
+                </div>
+                <div className="card-body">
+                    <h2 className="card-title font-bold md:text-xl sm:text-lg lg:font-extrabold lg:text-3xl text-purple-600 uppercase">{`${company_name} ${car_model} ${model_year}`}</h2>
+
+                    <div className='flex justify-start text-orange-400 md:font-semibold lg:font-bold md:text-lg uppercase'>
+                        <h3 className="lg:mr-5">Version: {model_year}</h3>
+                        <h3 className="lg:ms-5"> COLOR: {car_color}</h3>
+                    </div>
+
+                    <div>
+                        <h3 className='text-rose-600 md:font-semibold lg:font-bold lg:text-xl'>Price: {car_price}</h3>
+
+                        <p className='text-amber-900 md:font-semibold lg:font-bold lg:text-lg lg:my-3'>Vehicle Identification Number: {car_vin}</p>
+
+                        <p className=' text-indigo-900 mb-3 whitespace-pre-line'> <span className='text-red-500'>Description:</span> {` 
+                        " ${description} "`}</p>
+
+                    </div>
+                    <div className='md:font-semibold lg:font-bold lg:text-xl border-2 text-center border-teal-600 flex justify-evenly py-2 bg-white'>
+                        <h3 className='text-rose-600 '>Available: {quantity}</h3>
+                        {
+                            quantity > 0 ?
+                                <button className="btn btn-sm btn-success">Available</button> :
+                                <button className="btn btn-sm btn-disabled" >Stock Out</button>
+                        }
+                    </div>
+
+                    <div className="card-actions justify-between">
+                        <div>
+                            <h2 className='text-lg text-rose-500 '>Provider: <span className='text-indigo-700 hover:underline cursor-pointer'>{provider_name}</span></h2>
+                            <h2 className='text-lg text-rose-500'>Email: <span className='text-indigo-700'>{provider_email}</span></h2>
+                        </div>
+                        <Link to={`/item/${_id}`}>
+                            <button className="btn btn-warning rounded-2xl px-6 text-white font-semibold tracking-wider hover:bg-red-400" >Update</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BrandItem;
--- src/Pages/BrandItems/BrandItems.jsx
+++ src/Pages/BrandItems/BrandItems.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
+import BrandItem from './BrandItem';
 
 const BrandItems = () => {
-    const brand = useLoaderData()
-    console.log(brand);
+
+    const items = useLoaderData()
+
     return (
         <div>
-            
+            {
+                items && items.map(item => <BrandItem
+                    key={item._id}
+                    item={item}
+                ></BrandItem>)
+            }
         </div>
     );
 };
