dab37089ba5ebd35d0e0b944e1c1eb9c78d47ba2
khaled39ab <khaled39ab@gmail.com>
2023-03-26 20:12:08 +0600

    complete project

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -27,16 +27,6 @@ const Header = () => {
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         <li><Link to={'/store'}>Store</Link></li>
                         <li><Link to={'/blogs'}>Blogs</Link></li>
-                        {/* <li tabIndex={0}>
-                            <Link className="justify-between">
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                            </Link>
-                            <ul className="p-2">
-                                <li><Link>Submenu 1</Link></li>
-                                <li><Link>Submenu 2</Link></li>
-                            </ul>
-                        </li> */}
                         <li><Link to={'/contact'}>Contact us</Link></li>
                     </ul>
                 </div>
@@ -49,16 +39,6 @@ const Header = () => {
                 <ul className="menu menu-horizontal px-1">
                     <li><Link to={'/inventory'}>Inventory</Link></li>
                     <li><Link to={'/blogs'}>Blogs</Link></li>
-                    {/* <li tabIndex={0}>
-                        <Link>
-                            Parent
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </Link>
-                        <ul className="p-2">
-                            <li><Link>Submenu 1</Link></li>
-                            <li><Link>Submenu 2</Link></li>
-                        </ul>
-                    </li> */}
                     <li><Link to={'/contact'}>Contact us</Link></li>
                 </ul>
             </div>
